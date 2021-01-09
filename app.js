052312a2a416490df63e843f945216d7cf0fb4f3
Bogdan Tamas <bogdan.tamas@intern.arobs.com>
2021-01-09 18:08:45 +0200

    fix error

--- app.js
+++ app.js
@@ -215,7 +215,7 @@ function handleDialogFlowAction(sender, action, messages, contexts, parameters) 
     switch (action) {
         case "detailed-application":
             let filteredContexts = contexts.filter(function (el) {
-                return el.name.includes('job_application') ||
+                return el.name.includes('jobapplication') ||
                     el.name.includes('job-application-details_dialog_context')
             });
             if (filteredContexts.length > 0 && contexts[0].parameters) {
